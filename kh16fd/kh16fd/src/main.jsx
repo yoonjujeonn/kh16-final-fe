c19a0c6fcbf4d4e7bc97e81f36aab50d0aa66fb3
hansoobin-11 <vcoart@naver.com>
2025-12-04 11:16:45 +0900

    최초 프로젝트 생성(import 후 npm install)

---
+++ kh16fd/kh16fd/src/main.jsx
@@ -0,0 +1,22 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+//bootstrap + bootswatch css
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootswatch/dist/minty/bootstrap.min.css";
+
+//bootstrap js
+//import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap";
+//custom css
+import './index.css'
+
+//axios
+import "./utils/axios"
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
