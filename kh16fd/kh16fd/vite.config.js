74f178967ff91405991031e93b55aa42f50e22ca
kim9709 <kdk9709@naver.com>
2025-12-09 16:08:03 +0900

    카테고리 등록, 리스트에서 (수정+삭제)

--- kh16fd/kh16fd/vite.config.js
+++ kh16fd/kh16fd/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  //외부 접속 허용
+  server: {
+      host: "0.0.0.0",
+      port: 5173,
+  },
+
   plugins: [react()],
+
+  define: {
+    global: "window",//global이라는 키워드가 window를 가리키도록 별칭 설정
+  }
 })
