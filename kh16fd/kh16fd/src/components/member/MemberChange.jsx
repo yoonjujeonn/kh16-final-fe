40b670b4fbb205fe74acbc17de439cba4179a322
charlie098 <charlie09811@gmail.com>
2025-12-11 14:18:08 +0900

    마이페이지 변경 작업 중

--- kh16fd/kh16fd/src/components/member/MemberChange.jsx
+++ kh16fd/kh16fd/src/components/member/MemberChange.jsx
@@ -1,8 +1,29 @@
+import { useAtom } from "jotai";
+import { loginIdState, loginLevelState } from "../../utils/jotai";
 import Jumbotron from "../templates/Jumbotron";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 export default function MemberChange() {
-    return (<>
-        <Jumbotron subject="님의 정보 수정"/>
     
+    //state
+    const[loginId, setloginId] = useAtom(loginIdState);
+    const[loginLevel, setLoginLevel] = useAtom(loginLevelState);
+    
+    const [member, setMember] = useState(null);
+
+
+    return (<>
+        <Jumbotron subject={`안녕하세요 ${loginLevel}님`} detail={`${loginId}님의 정보 수정을 진행해 주세요`}/>
+        
+        <div className="row mt-4">
+            <div className="col">
+
+
+            </div>
+        </div>
+
     </>)
 }
